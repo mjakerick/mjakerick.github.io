a946e76845f79c5b2796f484dc18f0bfe813531f
Jake <mjakerick@gmail.com>
2019-04-15 12:44:52 -0700

    added carousel arrow images and pulled pokemon name and sprite info for two additional pokemon from API

--- Pokémon_Battle_Game/app.js
+++ Pokémon_Battle_Game/app.js
@@ -1,18 +1,50 @@
 $(() => {
 
+
+
   $.ajax({
     url:'https://pokeapi.co/api/v2/pokemon/1/'
-}).then(
+  }).then(
     (data)=>{
-        $('#name').html(data.name);
-        let pokemonSprite = $('<img>')
+        $('.poke1').html(data.name);
+        let pokemonSprite1 = $('<img>')
         .attr('src', data.sprites.front_default)
         .addClass('sprite')
-        $('#sprite').append(pokemonSprite);
+        $('.poke1').append(pokemonSprite1);
     },
     ()=>{
         console.log('bad request');
     }
-);
+  );
+
+  $.ajax({
+    url:'https://pokeapi.co/api/v2/pokemon/4/'
+  }).then(
+    (data)=>{
+        $('.poke2').html(data.name);
+        let pokemonSprite2 = $('<img>')
+        .attr('src', data.sprites.front_default)
+        .addClass('sprite')
+        $('.poke2').append(pokemonSprite2);
+    },
+    ()=>{
+        console.log('bad request');
+    }
+  );
+
+  $.ajax({
+    url:'https://pokeapi.co/api/v2/pokemon/7/'
+  }).then(
+    (data)=>{
+        $('.poke3').html(data.name);
+        let pokemonSprite3 = $('<img>')
+        .attr('src', data.sprites.front_default)
+        .addClass('sprite')
+        $('.poke3').append(pokemonSprite3);
+    },
+    ()=>{
+        console.log('bad request');
+    }
+  );
 
 })
