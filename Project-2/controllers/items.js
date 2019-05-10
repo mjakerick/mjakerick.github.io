16fa808b722d47894f45e12c235401864e5d71d3
Jake <mjakerick@gmail.com>
2019-05-10 07:08:09 -0700

    removed unnecessary folders

--- Project-2/controllers/items.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const Item = require('../models/items.js');
-
-router.get('/', (req, res) => {
-  res.render('index.ejs')
-});
-
-module.exports = router;
